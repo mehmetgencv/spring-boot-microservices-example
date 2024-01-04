a9d8caf30a5e62b1364dc554f324a395af656bd4
mehmetgencv <mehmetgencv@gmail.com>
2024-01-05 00:04:00 +0300

    add fraud rest template

--- customer/src/main/java/org/mehmetgenc/customer/service/CustomerService.java
+++ customer/src/main/java/org/mehmetgenc/customer/service/CustomerService.java
@@ -1,19 +1,37 @@
 package org.mehmetgenc.customer.service;
 
 import org.mehmetgenc.customer.dto.CustomerRegistrationRequest;
+import org.mehmetgenc.customer.dto.FraudCheckResponse;
 import org.mehmetgenc.customer.model.Customer;
 import org.mehmetgenc.customer.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
+    public CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate) {
+        this.customerRepository = customerRepository;
+        this.restTemplate = restTemplate;
+    }
+
+
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+        FraudCheckResponse fraudCheckResponse =  restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId());
+
+        if(fraudCheckResponse.isFraudster()){
+            throw new IllegalStateException("fraudester");
+        }
     }
 
 }
